6b729dcdc51c11409f977fe64c8f5fcaaa271e3c
Yerin <e_bbun_bul@naver.com>
2017-11-24 22:45:44 +0900

    금액수정

--- app/src/main/java/com/example/pc/testoink/DailyMoneySet.java
+++ app/src/main/java/com/example/pc/testoink/DailyMoneySet.java
@@ -25,7 +25,7 @@ import io.realm.Realm;
 import io.realm.RealmResults;
 
 
-public class DailyMoneySet extends AppCompatActivity implements View.OnClickListener{
+public class DailyMoneySet extends AppCompatActivity implements View.OnClickListener {
 
 
     private Realm mRealm;
@@ -44,8 +44,7 @@ public class DailyMoneySet extends AppCompatActivity implements View.OnClickList
     private AlertDialog.Builder overlap;//날짜가 겹치는 다이얼 로그가 있을 때 뜨는 다이얼로그
 
 
-
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dailymoneyset);
 
@@ -55,7 +54,7 @@ public class DailyMoneySet extends AppCompatActivity implements View.OnClickList
         startDate = (EditText) findViewById(R.id.startdate);
         endDate = (EditText) findViewById(R.id.enddate);
         setMoney = (EditText) findViewById(R.id.setMoney);
-        Aim = (EditText)findViewById(R.id.setAim);
+        Aim = (EditText) findViewById(R.id.setAim);
 
         startDate.setInputType(InputType.TYPE_NULL);
         startDate.requestFocus();
@@ -73,7 +72,7 @@ public class DailyMoneySet extends AppCompatActivity implements View.OnClickList
                 startDate.setText(dateFormatter.format(newDate.getTime()));
             }
 
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
         endDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
 
@@ -83,162 +82,158 @@ public class DailyMoneySet extends AppCompatActivity implements View.OnClickList
                 endDate.setText(dateFormatter.format(newDate.getTime()));
             }
 
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
-
-
-
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
 
     } //end oncreate
 
     //설정버튼 이벤트
-    public void btnSet(View v){
-if(v.getId()==R.id.setButton) {
-    //입력 다 안했을 때 뜨는 다이얼로그
-    subDialog = new AlertDialog.Builder(DailyMoneySet.this)
-            .setMessage("모두 입력해주세요")
-            .setCancelable(false)
-            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dlg2, int which) {
-                    dlg2.cancel();
-                }
-            });
+    public void btnSet(View v) {
+        if (v.getId() == R.id.setButton) {
+            //입력 다 안했을 때 뜨는 다이얼로그
+            subDialog = new AlertDialog.Builder(DailyMoneySet.this)
+                    .setMessage("모두 입력해주세요")
+                    .setCancelable(false)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dlg2, int which) {
+                            dlg2.cancel();
+                        }
+                    });
 
 
-    dateErrorDialog = new AlertDialog.Builder(DailyMoneySet.this)
-            .setMessage("종료날짜를 다시 설정해 주세요")
-            .setCancelable(false)
-            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dlg2, int which) {
-                    dlg2.cancel();
-                }
-            });
+            dateErrorDialog = new AlertDialog.Builder(DailyMoneySet.this)
+                    .setMessage("종료날짜를 다시 설정해 주세요")
+                    .setCancelable(false)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dlg2, int which) {
+                            dlg2.cancel();
+                        }
+                    });
 
-    overlap = new AlertDialog.Builder(DailyMoneySet.this)
-            .setMessage("겹치는 날짜가 존재합니다")
-            .setCancelable(false)
-            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dlg2, int which) {
-                    dlg2.cancel();
-                }
-            });
-
-
-    if (!Utility.isBlankField(setMoney) && !Utility.isBlankField(Aim)
-            && !Utility.isBlankField(startDate) && !Utility.isBlankField(endDate)) {
-        //금액 가져오기
-        String daily_money = setMoney.getText().toString();
-        String AimName = Aim.getText().toString();
-        int dailymoney = Integer.parseInt(daily_money);
-        //String start_date=startDate.getText().toString();
-        //String end_date=endDate.getText().toString();
+            overlap = new AlertDialog.Builder(DailyMoneySet.this)
+                    .setMessage("겹치는 날짜가 존재합니다")
+                    .setCancelable(false)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dlg2, int which) {
+                            dlg2.cancel();
+                        }
+                    });
 
 
-        //시작날짜 종료날찌 확인
-        try {
-            Date d_s = new SimpleDateFormat("yyyy-M-d").parse(startDate.getText().toString());
-            Date d_e = new SimpleDateFormat("yyyy-M-d").parse(endDate.getText().toString());
-            //Log.e("ee", d.toString()+"날짜 date변");
-            if (d_s.compareTo(d_e) >= 0) {
+            if (!Utility.isBlankField(setMoney) && !Utility.isBlankField(Aim)
+                    && !Utility.isBlankField(startDate) && !Utility.isBlankField(endDate)) {
+                //금액 가져오기
+                String daily_money = setMoney.getText().toString();
+                String AimName = Aim.getText().toString();
+                int dailymoney = Integer.parseInt(daily_money);
+                //String start_date=startDate.getText().toString();
+                //String end_date=endDate.getText().toString();
 
-                dateErrorDialog.show();
-            } else {
+
+                //시작날짜 종료날찌 확인
+                try {
+                    Date d_s = new SimpleDateFormat("yyyy-M-d").parse(startDate.getText().toString());
+                    Date d_e = new SimpleDateFormat("yyyy-M-d").parse(endDate.getText().toString());
+                    //Log.e("ee", d.toString()+"날짜 date변");
+                    if (d_s.compareTo(d_e) >= 0) {
+
+                        dateErrorDialog.show();
+                    } else {
 
 
-                //중복방지
-                int count = 0; //중복방지 변수
-                RealmResults<DailyMoneyModel> results = mRealm.where(DailyMoneyModel.class)
-                        .findAll();
-                mRealm.beginTransaction();
-                for (int i = 0; i < results.size(); i++) {
+                        //중복방지
+                        int count = 0; //중복방지 변수
+                        RealmResults<DailyMoneyModel> results = mRealm.where(DailyMoneyModel.class)
+                                .findAll();
+                        mRealm.beginTransaction();
+                        for (int i = 0; i < results.size(); i++) {
 
-                    if (results.get(i).getstartDate().compareTo(d_s) <= 0 &&            //resualts.startdate < d_s < resualts.EndDate
-                            d_s.compareTo(results.get(i).getEndDate()) <= 0) {
-                        count++;
-                        break;
-                    } else if (results.get(i).getstartDate().compareTo(d_e) <= 0 &&      //resualts.startdate < d_e < resualts.EndDate
-                            d_e.compareTo(results.get(i).getEndDate()) <= 0) {
-                        count++;
-                        break;
-                    } else if (d_s.compareTo(results.get(i).getstartDate()) <= 0 &&     //d_s<resualts.startdate<d_e
-                            results.get(i).getstartDate().compareTo(d_e) <= 0) {
-                        count++;
-                        break;
+                            if (results.get(i).getstartDate().compareTo(d_s) <= 0 &&            //resualts.startdate < d_s < resualts.EndDate
+                                    d_s.compareTo(results.get(i).getEndDate()) <= 0) {
+                                count++;
+                                break;
+                            } else if (results.get(i).getstartDate().compareTo(d_e) <= 0 &&      //resualts.startdate < d_e < resualts.EndDate
+                                    d_e.compareTo(results.get(i).getEndDate()) <= 0) {
+                                count++;
+                                break;
+                            } else if (d_s.compareTo(results.get(i).getstartDate()) <= 0 &&     //d_s<resualts.startdate<d_e
+                                    results.get(i).getstartDate().compareTo(d_e) <= 0) {
+                                count++;
+                                break;
 
-                    } else if (d_s.compareTo(results.get(i).getEndDate()) <= 0 &&        //d_s<resualts.Enddate<d_e
-                            results.get(i).getEndDate().compareTo(d_e) <= 0) {
-                        count++;
-                        break;
+                            } else if (d_s.compareTo(results.get(i).getEndDate()) <= 0 &&        //d_s<resualts.Enddate<d_e
+                                    results.get(i).getEndDate().compareTo(d_e) <= 0) {
+                                count++;
+                                break;
+                            }
+
+
+                        }
+                        mRealm.commitTransaction();
+
+
+                        if (count > 0) {
+                            overlap.show();
+                        } else {
+
+                            //데이터베이스에 추가하기
+                            mRealm.beginTransaction();
+
+                            DailyMoneyModel DM = mRealm.createObject(DailyMoneyModel.class);
+                            DM.setAimName(AimName);
+                            DM.setMoney_set(dailymoney);
+                            DM.setStartDate(d_s);
+                            DM.setEndDate(d_e);
+
+                            mRealm.commitTransaction();
+
+                            Log.d("ee", AimName + "  " + dailymoney + "  ");
+
+
+                            //메인으로 돌아가기
+                            Intent intent = new Intent(getApplicationContext(),//현재화면의
+                                    MainActivity.class);//다음 넘어갈 클래스 지정
+
+                            startActivity(intent);//다음 화면으로 넘어간다
+                            finish();
+
+
+                        }
+
+
                     }
 
-
-                }
-                mRealm.commitTransaction();
-
-
-                if (count > 0) {
-                    overlap.show();
-                } else {
-
-                    //데이터베이스에 추가하기
-                    mRealm.beginTransaction();
-
-                    DailyMoneyModel DM = mRealm.createObject(DailyMoneyModel.class);
-                    DM.setAimName(AimName);
-                    DM.setMoney_set(dailymoney);
-                    DM.setStartDate(d_s);
-                    DM.setEndDate(d_e);
-
-                    mRealm.commitTransaction();
-
-                    Log.d("ee", AimName + "  " + dailymoney + "  ");
-
-
-                    //메인으로 돌아가기
-                    Intent intent = new Intent(getApplicationContext(),//현재화면의
-                            MainActivity.class);//다음 넘어갈 클래스 지정
-
-                    startActivity(intent);//다음 화면으로 넘어간다
-                    finish();
-
-
+                } catch (ParseException e) {
+                    e.printStackTrace();
                 }
 
+
+            } else {
+
+                subDialog.show();
 
             }
+        } else if (v.getId() == R.id.btn_can) {
 
-        } catch (ParseException e) {
-            e.printStackTrace();
+            // 전화면
+            Intent intent = new Intent(getApplicationContext(),//현재화면의
+                    CalenderDialog.class);//다음 넘어갈 클래스 지정
+
+            startActivity(intent);//다음 화면으로 넘어간다
+            finish();
         }
-
-
-    } else {
-
-        subDialog.show();
-
-    }
-}else if (v.getId()==R.id.btn_can){
-
-    // 전화면
-    Intent intent = new Intent(getApplicationContext(),//현재화면의
-            CalenderDialog.class);//다음 넘어갈 클래스 지정
-
-    startActivity(intent);//다음 화면으로 넘어간다
-    finish();
-}
 
     } // end btnset
 
 
-
     @Override
     public void onClick(View view) {
-        if(view == startDate) {
+        if (view == startDate) {
             startDatePickerDialog.show();
-        } else if(view == endDate) {
+        } else if (view == endDate) {
             endDatePickerDialog.show();
         }
     }
@@ -249,7 +244,7 @@ if(v.getId()==R.id.setButton) {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
 
-        if(event.getKeyCode() == KeyEvent.KEYCODE_BACK){
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
 
             // 전화면
             Intent intent = new Intent(getApplicationContext(),//현재화면의
@@ -261,6 +256,7 @@ if(v.getId()==R.id.setButton) {
         return true;
 
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
--- app/src/main/java/com/example/pc/testoink/MainActivity.java
+++ app/src/main/java/com/example/pc/testoink/MainActivity.java
@@ -51,19 +51,19 @@ public class MainActivity extends AppCompatActivity {
     final static String LOG_TAG = "myLogs";
 
     //그 전 화면의 intent에서 date정보를 가져오는 함수
-    static String intent_setDate;
+    static String intent_setDate=null;
 
 
 
     /* 뷰 */
-    private TextView mTxtPercent; // 달성률 __ click 금액 다이얼로그
-    private TextView mRestPercent; //회전 후 나오는거
-    private TextView mTxtCalcDate; // 오늘날짜 __ click 달력 다이얼로그
+    private TextView mTxtPercent = null; // 달성률 __ click 금액 다이얼로그
+    private TextView mRestPercent = null; //회전 후 나오는거
+    private TextView mTxtCalcDate = null; // 오늘날짜 __ click 달력 다이얼로그
     private ImageView mImgScrEvent; // 스크롤 이벤트 버튼
     private FloatingActionButton mBtnAddUpdate; // 플로팅 __ click 수입,지출 기록 다이얼로그
     private FloatingActionButton mBtnMoneySet; // 플로팅 __ click 일일 지출액 설정
-    private TextView dailyset;
-    private TextView remMoney;
+    private TextView dailyset =  null;
+    private TextView remMoney = null;
 
     /*  private CustomCalendarDialog mCalendarDialog= new CustomCalendarDialog(this);
 */
@@ -145,8 +145,8 @@ public class MainActivity extends AppCompatActivity {
         dailyset=(TextView)findViewById(R.id.txt_dailySet);
         remMoney=(TextView)findViewById(R.id.txt_remain);
 
-        dataAdapter=new DataDetailsAdapter(this,dataDetailsModelArrayList);
-        mIncExpList.setAdapter(dataAdapter);
+        //dataAdapter=new DataDetailsAdapter(this,dataDetailsModelArrayList);
+        //mIncExpList.setAdapter(dataAdapter);
 
         scrollView = (ScrollView) findViewById(R.id.ScrollView);
         setmoney = 0;
@@ -208,6 +208,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //새로추가를 눌렀을 때 null을 줌
                 addOrUpdatePersonDetailsDialog(null, -1);
+                dataDetailsAdapter.notifyDataSetChanged();
+
             }
         });
 
@@ -256,6 +258,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
 ///--------------------db관련 함수들-----------------------
 
     public static MainActivity getInstance() {
@@ -283,6 +287,12 @@ public class MainActivity extends AppCompatActivity {
         myRealm.commitTransaction();
         dataDetailsAdapter.notifyDataSetChanged();
 
+        setMoney_sum();
+
+    }
+
+    public void setMoney_sum(){
+
         // 남은 금액 계산
         money_sum = 0;
         for (int j = 0; j < dataDetailsModelArrayList.size(); j++) {
@@ -295,6 +305,14 @@ public class MainActivity extends AppCompatActivity {
         }
         String string = Integer.toString(money_sum);
         Log.e("money", string);
+
+        if(mRestPercent != null)
+            mRestPercent.setText(money_sum + "원");
+        if(dailyset != null)
+            dailyset.setText(Integer.toString(setmoney));
+        if(remMoney != null)
+            remMoney.setText(Integer.toString(setmoney-money_sum));
+
     }
 
     public void deleteData(int personId, int position) {
@@ -336,6 +354,7 @@ public class MainActivity extends AppCompatActivity {
         myRealm.commitTransaction();
         id++;
         dataDetailsAdapter.notifyDataSetChanged();
+        setMoney_sum();
 
     } // end addDataToRealm
 
@@ -351,6 +370,7 @@ public class MainActivity extends AppCompatActivity {
         myRealm.commitTransaction();
         dataDetailsModelArrayList.set(position, editPersonDetails);
         dataDetailsAdapter.notifyDataSetChanged();
+        setMoney_sum();
     } // end updatePersonDetails
 
 
@@ -456,7 +476,7 @@ public class MainActivity extends AppCompatActivity {
                         dataDetailsModel.setInOrOut(true); //지출
 
 
-                        dataDetailsAdapter.notifyDataSetChanged();
+
 
                         Log.d("eeeeeeeeeeeeeeeeeeee", dataDetailsModel.getDate().toString());
 
@@ -469,6 +489,8 @@ public class MainActivity extends AppCompatActivity {
                 } else {//다이얼 로그가 비워져 있다면 이것을 보여줘!!
                     subDialog.show();
                 }
+
+                dataDetailsAdapter.notifyDataSetChanged();
             }
         });
 
@@ -573,6 +595,9 @@ public class MainActivity extends AppCompatActivity {
                     mContainer.startAnimation(rot);
                 }
             });
+
+
+            dataDetailsAdapter.notifyDataSetChanged();
         }
 
         @Override
