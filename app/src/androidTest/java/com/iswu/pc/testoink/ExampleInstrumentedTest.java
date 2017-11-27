b73500df4babde7e04206da5dd4a5f99d476b6c5
Yerin <e_bbun_bul@naver.com>
2017-11-27 17:40:54 +0900

    패키

--- app/src/androidTest/java/com/iswu/pc/testoink/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/iswu/pc/testoink/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.pc.testoink", appContext.getPackageName());
+        assertEquals("com.iswu.pc.testoink", appContext.getPackageName());
     }
 }
